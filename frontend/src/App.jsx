1c371f8558b93b9d48cba986098ecd039bf7a845
drtapse7 <153412172+drtapse7@users.noreply.github.com>
2024-02-08 09:38:06 +0530

    created loginform component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import LoginForm from './components/LoginForm'
 
 function App() {
 
   return (
     <>
        <h1>Welcome to eRozgaar connect</h1> 
+       <LoginForm/>
     </>
   )
 }
---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,12 @@
+
+
+export default function LoginForm(props) {
+  
+ 
+
+  return (
+    <div>
+        <h1>Welcome to CDAC Project</h1>
+    </div>
+  );
+}
