062320852d6d2873ab4fe5307f4946a08c3a69a9
Dhananjay Yelwande <yelwandedhananjay@gmail.com>
2024-02-10 00:25:00 +0530

    Update UserController and application.properties, and modify WorkerRegistrationForm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 //import './App.css'
-import LoginForm from './components/forms/LoginForm'
-// import WorkerRegistrationForm from './components/forms/WorkerRegistrationForm'
+//import LoginForm from './components/forms/LoginForm'
+import WorkerRegistrationForm from './components/forms/WorkerRegistrationForm'
 
 function App() {
 
   return (
     <>
         {/* <h1>Welcome to eRozgaar connect</h1>  */}
-        <LoginForm/> 
+        <WorkerRegistrationForm/> 
        {/* <WorkerRegistrationForm/> */}
     </>
   )
