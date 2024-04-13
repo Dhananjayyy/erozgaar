966e6de996f29aacea512ceed668c51f835804bd
Dhananjay Yelwande <yelwandedhananjay@gmail.com>
2024-04-14 00:25:34 +0530

    Fix SpringBootTest annotation

--- backend/springboot/erozgaar/src/test/java/com/example/demo/SpringSecurityPractice3ApplicationTests.java
+++ backend/springboot/erozgaar/src/test/java/com/example/demo/SpringSecurityPractice3ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringSecurityPractice3ApplicationTests.class)
 class SpringSecurityPractice3ApplicationTests {
 
 	@Test
