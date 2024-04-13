361c06fb6a86bb373a061b82c039f41b4ec7fe89
Dhananjay Yelwande <yelwandedhananjay@gmail.com>
2024-04-14 02:08:26 +0530

    Add GET endpoint for /hello in ErozgaarApplication.java

--- backend/springboot/erozgaar/src/main/java/com/knowit/erozgaar/ErozgaarApplication.java
+++ backend/springboot/erozgaar/src/main/java/com/knowit/erozgaar/ErozgaarApplication.java
@@ -3,11 +3,17 @@ package com.knowit.erozgaar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.knowit.erozgaar.*")
 public class ErozgaarApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ErozgaarApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World";
 	}
 }
