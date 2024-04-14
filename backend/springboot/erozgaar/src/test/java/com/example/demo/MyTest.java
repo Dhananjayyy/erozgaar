66e720818606679fc132f3351bcb7c95498b0ded
Dhananjay Yelwande <yelwandedhananjay@gmail.com>
2024-04-14 17:09:55 +0530

    Delete backend directory

--- backend/springboot/erozgaar/src/test/java/com/example/demo/MyTest.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-
-@SpringBootTest
-@SpringJUnitConfig
-public class MyTest {
-    // Test methods
-}
